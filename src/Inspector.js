1ffe9ab3dd434969d5e29a2dd15eda89c8583654
Kevin Barabash <kevinb@khanacademy.org>
2017-03-19 15:29:31 -0400

    started work to add palettes and editing to the inspector

--- src/Inspector.js
+++ src/Inspector.js
@@ -1,19 +1,114 @@
 // @flow
-import React, {Component} from 'react'
+import React, {Children, Component} from 'react'
 
 import defaultStyle from './default-style'
 import type {Node} from './types'
 
-const Swatch = ({ color }) => <div style={{ display: 'inline-block', width: 12, height: 12, backgroundColor: color, marginRight: 2, borderRadius: 2 }} />;
+const swatchStyle = {
+    display: 'inline-block',
+    width: 12,
+    height: 12,
+    marginRight: 3,
+    marginTop: 3,
+    borderRadius: 2,
+};
+
+const Swatch = ({ color }) => <div
+    style={{ ...swatchStyle, backgroundColor: color }}
+/>;
+
+const values = {
+    flexDirection: ['column', 'column-reverse', 'row', 'row-reverse'],
+    alignItems: ['flex-start', 'flex-end', 'center', 'space-around', 'space-between'],
+    justifyContent: ['flex-start', 'flex-end', 'center', 'space-around', 'space-between'],
+}
+
+const units = ['em', 'ex', '%', 'px', 'cm', 'mm', 'in', 'pt', 'pc'];
+
+const parseLength = (str) => {
+
+}
+
+type Length = {
+    value: number,
+    unit: 'em' | 'ex' | '%' | 'px' | 'cm' | 'mm' | 'in' | 'pt' | 'pc',
+}
+
+// What's the source of truth?
+// We could have a struct for these values and always update that
+const shorthand = {
+    flex: {
+        children: ['flexGrow', 'flexShrink', 'flexBasis'],
+        // TODO(kevinb) validator function
+        // TODO(kevinb) generate short hand from children
+        // TODO(kevinb) generate children from editing shorthand
+    },
+
+    margin: {
+        children: [''],
+        toShorthand: (top, right, bottom, left) => `${top}px ${right}px ${bottom}px ${right}px`,
+        toLonghand: (shorthand) => {
+            const [top, right, bottom, left] = shorthand.split(' ').map(parseFloat);
+            return {top, right, bottom, left};
+        },
+    }
+}
+
+const groups = {
+    flex: [
+        'flexDirection', 'flex', 'justifyContent', 'alignItems',
+    ],
+    dim: [
+        'margin', 'padding',
+    ],
+}
+
+class SmallButton extends Component {
+    props: {
+        children?: Children,
+    }
+
+    state = {
+        hovered: false,
+    }
+
+    render() {
+        return <div
+            onMouseEnter={() => this.setState({hovered: true})}
+            onMouseLeave={() => this.setState({hovered: false})}
+            style={{
+                display: 'inline-block',
+                padding: `2px 4px`,
+                borderRadius: 4,
+                borderStyle: 'solid',
+                borderWidth: 1,
+                borderColor: this.state.hovered ? 'gray' : 'lightgray',
+                fontFamily: 'sans-serif',
+                color: this.state.hovered ? 'gray' : 'lightgray',
+                fontSize: 13,
+                marginRight: 4,
+                cursor: this.state.hovered ? 'pointer' : 'initial',
+            }}
+        >
+            {this.props.children}
+        </div>;
+    }
+}
 
 export default class Inspector extends Component {
     props: {
         data: Node,
     }
 
-    formatValue(value: any) {
+    formatValue(key: string, value: any) {
         if (typeof value === 'number') {
             return `${value}px`;  // TODO(kevinb) handle more units
+        } else if (key in values) {
+            return <select>
+                {values[key].map((value) => <option key={value} value={value}>
+                    {value}
+                </option>)}
+            </select>;
         } else {
             return value;
         }
@@ -27,18 +122,25 @@ export default class Inspector extends Component {
             ...data.style,
         }
 
+        // TODO(kevinb) split this into rendered and editors
+        // When focusing a cell we render the editor
+        // When bluring a cell we validate, update if valid, and render the viewer
         return <div style={{ width: 300 }}>
-            <table>
+            <SmallButton>flex</SmallButton>
+            <SmallButton>dim</SmallButton>
+            <SmallButton>border</SmallButton>
+            <SmallButton>text</SmallButton>
+            <table style={{ borderCollapse: 'collapse', borderSpacing: 0, width: '100%', marginTop: 8 }}>
                 <tbody>
                     {Object.keys(style).sort().map(key => {
                         const value = style[key]
                         const isColor = /color/i.test(key)
 
-                        return <tr style={{ color: defaultStyle[key] !== value ? 'black' : '#AAA' }}>
-                            <td style={{ fontFamily: 'monospace', paddingRight: 16 }}>{key}</td>
-                            <td style={{ display: 'flex', alignItems: 'center', fontFamily: 'monospace' }}>
+                        return <tr key={key} style={{ color: defaultStyle[key] !== value ? 'black' : '#AAA' }}>
+                            <td style={{ fontFamily: 'monospace', paddingRight: 16, border: 'solid 1px #CCC', padding: '2px 4px'}}>{key}</td>
+                            <td style={{ alignItems: 'center', fontFamily: 'monospace', border: 'solid 1px #CCC', padding: '2px 4px'}}>
                                 {isColor && <Swatch color={value} />}
-                                {this.formatValue(value)}
+                                {this.formatValue(key, value)}
                             </td>
                         </tr>
                     })}
--- src/default-style.js
+++ src/default-style.js
@@ -13,6 +13,16 @@ const defaultStyle = {
     boxSizing: 'border-box',
 
     fontWeight: 'normal',
+
+    marginTop: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    marginLeft: 0,
+
+    paddingTop: 0,
+    paddingRight: 0,
+    paddingBottom: 0,
+    paddingLeft: 0,
 }
 
 export default defaultStyle;
