9008968a5789516d26f78f8e8bc7743b1d8ffa6c
Kevin Barabash <kevinb@khanacademy.org>
2017-03-21 22:08:44 -0400

    initial redux setup

--- src/App.js
+++ src/App.js
@@ -1,101 +1,25 @@
 // @flow
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
 import Inspector from './Inspector'
 import Renderer from './Renderer'
 
-// TODO(kevinb) in the UI allow @name to reference constants like colors and such
-
-// TODO(kevinb) have different palettes that you can switch between for flex,
-// margin, padding, border, font, colors, etc.
-
-const typography = {
-  subheading: {
-    fontFamily: 'sans-serif',
-    fontSize: 23,
-    fontWeight: 'bold',
-  },
-  body: {
-    fontFamily: 'sans-serif',
-    fontSize: 16,
-    fontWeight: 'normal', // dedupe from defaultStyles
-  },
-};
-
-const card = (props) => ({
-  tag: 'div',
-  style: {
-    width: 300,
-    height: 100,
-    justifyContent: 'flex-start',
-    backgroundColor: 'pink',
-    borderRadius: 4,
-    borderStyle: 'solid',
-    borderColor: 'gray',
-    padding: 16,
-    flexDirection: 'row',
-    alignItems: 'center',
-
-    // TODO(kevinb) figure out how to apply this to only the first item
-    marginTop: 16,
-  },
-  children: [
-    {
-      tag: 'img',
-      style: {
-        height: 64,
-        width: 64,
-      },
-      src: 'http://placekitten.com/g/64/64',
-    },
-    {
-      tag: 'div',
-      style: {
-        marginLeft: 12,
-      },
-      children: [
-        {
-          tag: 'div',
-          style: {
-            ...typography.subheading,
-          },
-          children: `Hello, ${props.name}`,
-        },
-        {
-          tag: 'div',
-          style: {
-            ...typography.body,
-            marginTop: 4,
-          },
-          children: 'Meow!',
-        },
-      ]
-    }
-  ],
-})
-
-// TODO(kevinb) figure out how to modify the number of cards shown
-const layout = (names) => ({
-  tag: 'div',
-  style: {
-
-  },
-  // TODO(kevinb) need a way to provide a mapping to create each of the props object
-  children: names.map((name) => card({name: name})),
-})
-
-
-const names = ['tabby', 'caleco', 'alley', 'blue', 'siamese'];
-
 
 class App extends Component {
+  props: {
+    data: any,
+  }
+
   render() {
+    const {data} = this.props;
+
     return <div>
       <h1>faster test page</h1>
       <div style={{display: 'flex', flexDirection: 'row'}}>
-        <Inspector data={card({name: 'james'})}/>
-        <Renderer data={card({name: 'james'})}/>
+        <Inspector data={data}/>
+        <Renderer data={data}/>
       </div>
     </div>
   }
@@ -104,5 +28,10 @@ class App extends Component {
 // the tree is all about indices of children
 // start with a tree separate from the element inspector
 
+const stateToProps = (state) => {
+  return {
+    data: state,
+  }
+}
 
-export default App;
+export default connect(stateToProps)(App);
---
+++ src/store.js
@@ -0,0 +1,89 @@
+// @flow
+import {createStore} from 'redux'
+
+// TODO(kevinb) in the UI allow @name to reference constants like colors and such
+
+// TODO(kevinb) have different palettes that you can switch between for flex,
+// margin, padding, border, font, colors, etc.
+
+const typography = {
+  subheading: {
+    fontFamily: 'sans-serif',
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontFamily: 'sans-serif',
+    fontSize: 16,
+    fontWeight: 'normal', // dedupe from defaultStyles
+  },
+};
+
+const initialState = {
+  tag: 'div',
+  style: {
+    width: 300,
+    height: 100,
+    justifyContent: 'flex-start',
+    backgroundColor: 'pink',
+    borderRadius: 4,
+    borderStyle: 'solid',
+    borderColor: 'gray',
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    // TODO(kevinb) figure out how to apply this to only the first item
+    marginTop: 16,
+  },
+  children: [
+    {
+      tag: 'img',
+      style: {
+        height: 64,
+        width: 64,
+      },
+      src: 'http://placekitten.com/g/64/64',
+    },
+    {
+      tag: 'div',
+      style: {
+        marginLeft: 12,
+      },
+      children: [
+        {
+          tag: 'div',
+          style: {
+            ...typography.subheading,
+          },
+          children: `Hello, world`, // TODO: how to inject props
+        },
+        {
+          tag: 'div',
+          style: {
+            ...typography.body,
+            marginTop: 4,
+          },
+          children: 'Meow!',
+        },
+      ]
+    }
+  ],
+}
+
+// TODO(kevinb) figure out how to modify the number of cards shown
+// const layout = (names) => ({
+//     tag: 'div',
+//     style: {
+//     },
+//     // TODO(kevinb) need a way to provide a mapping to create each of the props object
+//     children: names.map((name) => card({ name: name })),
+// })
+
+// const names = ['tabby', 'caleco', 'alley', 'blue', 'siamese'];
+
+const reducer = (state, action) => {
+    return state;
+}
+
+export default createStore(reducer, initialState)
